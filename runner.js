3ea0fdd735dea06058375828731b0acef9bb2d0f
Dende98 <danilopalov@gmail.com>
2025-09-19 15:19:15 +0300

    task 5 prettyBoard

--- runner.js
+++ runner.js
@@ -3,11 +3,10 @@ const { read, solve, isSolved, prettyBoard } = require('./sudoku');
 
 const puzzle = read();
 const solved = solve(puzzle);
-// const isSolvedFlag = isSolved(solved);
-// if (isSolvedFlag === true) {
-//   prettyBoard(puzzle, solved);
-//   console.log(true);
-// } else {
-//   console.log(false);
-// }
-console.log(solved);
+const isSolvedFlag = isSolved(solved);
+if (isSolvedFlag === true) {
+  prettyBoard(puzzle, solved);
+  console.log(true);
+} else {
+  console.log(false);
+}
